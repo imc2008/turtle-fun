9cf16433010f88e3bfefb2c37e4a08c244a187ae
imc2008 <katherineconway3@gmail.com>
2015-02-20 12:49:53 +0000

    i can spell IT

---
+++ I.py
@@ -0,0 +1,57 @@
+from turtle import*
+
+def nextLetter():
+    setx(xcor() + 100)
+
+def I():
+    forward(75)
+    right(90)
+    forward(25)
+    right(90)
+    forward(25)
+    left(90)
+    forward(100)
+    left(90)
+    forward(25)
+    right(90)
+    forward(25)
+    right(90)
+    forward(75)
+    right(90)
+    forward(25)
+    right(90)
+    forward(25)
+    left(90)
+    forward(100)
+    left(90)
+    forward(25)
+    right(90)
+    forward(25)
+    right(90)
+    nextLetter()
+
+def T():
+    forward(75)
+    right(90)
+    forward(25)
+    right(90)
+    forward(25)
+    left(90)
+    forward(100)
+    right(90)
+    forward(25)
+    right(90)
+    forward(100)
+    left(90)
+    forward(25)
+    right(90)
+    forward(25)
+    right(90)
+    nextLetter()
+
+
+I()
+T()
+
+
+
